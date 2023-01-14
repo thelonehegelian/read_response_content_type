410b9dea123dd8712f9b797955a408337e41af3a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 05:34:49 +0000

    Read response header and get mime type

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,27 @@ async fn main() -> Result<()> {
     let url = "https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2303&q=80.png";
     let client = reqwest::Client::new();
     let response = client.get(url).send().await?;
-    println!("Status: {:?}", response);
+
+    // get the content type
+    let content_type = response
+        .headers()
+        .get(CONTENT_TYPE)
+        .ok_or("No content type")?
+        .to_str()?;
+    let mime_type: Mime = Mime::from_str(content_type)?;
+
+    println!("Mime: {:?}", mime_type);
+
+    // Another method
+    // let header = response.headers();
+
+    // match header.get(CONTENT_TYPE) {
+    //     Some(content_type) => {
+    //         let mime_type: Mime = Mime::from_str(content_type.to_str()?)?;
+    //         println!("Mime: {:?}", mime_type);
+    //     }
+    //     None => println!("No content type"),
+    // }
 
     Ok(())
 }
